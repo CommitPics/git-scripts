84636fae45f1b10312c39525ba6f113d95efb32e
Ladislas <ladislas@weareleka.com>
2014-12-30 18:21:55 +0100

    finalize the install script - ask for username, remove git files after cloning and start brand new, catch exception and errors

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ while true; do
 	esac
 done
 
+echo "What is your Github username?"
+read GITHUB_USERNAME
+
 # Set global variables
 GITHUB_DOMAIN="https://github.com"
 COMMITFIE_URL="$GITHUB_DOMAIN/Commitfie/git-commitfie"
@@ -21,7 +24,7 @@ GIT_TEMPLATES_PATH="$HOME/.git-templates"
 GIT_HOOKS_PATH="$GIT_TEMPLATES_PATH/hooks"
 
 # Make sure all the brew dependencies are met
-BREW_DEPS=( imagesnap imagemagick whereami )
+BREW_DEPS=( git imagesnap imagemagick whereami )
 MISSING_BREW_DEPS=()
 
 echo "\nChecking for \`brew\` dependencies:\n "
@@ -41,20 +44,30 @@ if [  ${#MISSING_BREW_DEPS[@]} -gt 0 ] ; then
 fi
 
 # Create hooks
-echo "Creating $GIT_HOOKS_PATH..."
 if [[ ! -d  $GIT_HOOKS_PATH ]] ; then
+	echo "\nCreating $GIT_HOOKS_PATH..."
 	mkdir -p $GIT_HOOKS_PATH
 fi
 
 # Clone commitfie/git-commitfie to ~/.git-commitfie
 echo "\nCloning $COMMITFIE_URL to $COMMITFIE_PATH..."
 git clone $COMMITFIE_URL $COMMITFIE_PATH || exit 1
+
+# Initialize submodules
 cd $COMMITFIE_PATH
+echo "\nInitializing submodules in $PWD..."
 git submodule init && git submodule update || exit 1
 
+# Remove git files to start brand new
+echo "\nRemoving everything related to git and starting anew..."
+rm -rf .git .gitmodule
+git init
+git remote add origin $GITHUB_DOMAIN/$GITHUB_USERNAME/git-commitfie
+$(git remote show origin &> /dev/null ) || echo "\nATTENTION! We could not set properly the remote repo, you'll have to do it on your own."
+
 # Symlink the post-commit script
-echo "Symlinking $COMMITFIE_SCRIPT_PATH to $GIT_HOOKS_PATH/post-commit..."
-ln -s $COMMITFIE_SCRIPT_PATH $GIT_HOOKS_PATH/post-commit || exit 1
+echo "\nSymlinking $COMMITFIE_SCRIPT_PATH to $GIT_HOOKS_PATH/post-commit..."
+$(ln -s $COMMITFIE_SCRIPT_PATH $GIT_HOOKS_PATH/post-commit &> /dev/null) || $(echo "\nERROR! You already have a post-commit file. Remove it to continue the process" && exit 1)
 
 echo \
 "\nCongratulation! git-commitfie is now ready to be used!
