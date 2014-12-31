61b63340d67357ce1906bae287da6637934cbd21
Ladislas <ladislas@weareleka.com>
2014-12-31 20:04:34 +0100

    wip on variables, must add some error checking if empty - #2

--- commitfie.py
+++ commitfie.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Import modules
-# import os
+import os
 from subprocess import check_output
 from datetime import datetime, date
 # from subprocess import call
@@ -11,6 +11,9 @@ COMMITFIE_ROOT = "$HOME/.git-commitpics"
 COMMITFIE_IMG_DIR = "$COMMITFIE_ROOT/assets/img/commit"
 COMMITFIE_THUMB_DIR = "$COMMITFIE_ROOT/assets/img/commit/thumbs"
 COMMITFIE_POST_DIR = "$COMMITFIE_ROOT/_posts/commit"
+# COMMITFIE_PWD = check_output(["echo", ${PWD##*/}])
+# print COMMITFIE_PWD
+print os.path.dirname(os.path.realpath(__file__))
 
 # Set Github and local git variables
 GITHUB_DOMAIN = "https://github.com"
@@ -41,23 +44,16 @@ elif CURRENT_HOUR <= 23:
 
 # Get last commit info
 COMMIT_HASH = check_output(["git", "log", "-1", "--pretty=%h"])
+COMMIT_HASH_LONG = check_output(["git", "log", "-1", "--pretty=%H"])
+COMMIT_DATE = check_output(["git", "log", "-1", "--pretty=%ad"])
+COMMIT_MESSAGE = check_output(["git", "log", "-1", "--pretty=%s"])
+COMMIT_BRANCH = check_output(["git", "symbolic-ref", "--short", "HEAD"])
+COMMIT_REPOSITORY_ORIGIN = check_output(["git", "config", "--get", "remote.origin.url"])
+COMMIT_REPOSIROTY_NAME = COMMIT_REPOSITORY_ORIGIN.replace(GITHUB_DOMAIN + '/', '')
 
-# COMMIT_HASH=$(git log -1 --pretty="%h")
-# COMMIT_HASH_LONG=$(git log -1 --pretty="%H")
-# COMMIT_AUTHOR_NAME=$(git log -1 --pretty="%an")
-# COMMIT_AUTHOR_EMAIL=$(git log -1 --pretty="%ae")
-# COMMIT_DATE=$(git log -1 --pretty="%ad")
-# COMMIT_MESSAGE=$(git log -1 --pretty="%s")
-# COMMIT_REPOSITORY_URL=$(git config --get remote.origin.url)
-# # todo - create name from pwd if not url is set
-# COMMIT_REPOSITORY=$(echo $COMMIT_REPOSITORY_URL | sed "s/$GITHUB_DOMAIN\///g")
-# COMMIT_BRANCH=$(git symbolic-ref --short HEAD)
-# COMMIT_PICTURE="$NOW-${PWD##*/}-$COMMIT_HASH"
-# COMMIT_POST="$COMMIT_PICTURE.markdown"
-#
-# # Set picture and post names
-# COMMIT_PICTURE = NOW + "-" + PWD + "-" + COMMIT_HASH
-# COMMIT_POST="$COMMIT_PICTURE.markdown"
+# Set picture and post names
+COMMIT_PICTURE = NOW + "-" + PWD + "-" + COMMIT_HASH
+COMMIT_POST="$COMMIT_PICTURE.markdown"
 
 # Take the picture and create the thumbnail
 
