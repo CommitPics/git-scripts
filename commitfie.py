238d92af7a58d7d1d9cdc6956c84a8a1cdb9a35e
ladislas <ladislas@weareleka.com>
2015-01-03 13:37:24 +0100

    add global variables, sanitize the way we get command output, create custom commandcall function to handle errors, take picture and resize thumbnails - #2 - fixes #6

--- commitfie.py
+++ commitfie.py
@@ -2,26 +2,41 @@
 
 # Import modules
 import os
-from subprocess import check_output
+import subprocess
 from datetime import datetime, date
-# from subprocess import call
+
+# Global variables
+HOME = os.path.expanduser("~")
+CWD_PATH = os.getcwd()
+CWD = os.path.basename(CWD_PATH)
+
+# Function Definitions
+def commandCall(cmd):
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    if (process.returncode == 0):
+        out = out.replace('\n', '').replace('\r', '')
+        print out
+        return out
+    else:
+        err = err.replace('\n', ' ').replace('\r', '')
+        print err
+        return "n/a"
 
 # Set Commitfie variables
-COMMITFIE_ROOT = "$HOME/.git-commitpics"
-COMMITFIE_IMG_DIR = "$COMMITFIE_ROOT/assets/img/commit"
-COMMITFIE_THUMB_DIR = "$COMMITFIE_ROOT/assets/img/commit/thumbs"
-COMMITFIE_POST_DIR = "$COMMITFIE_ROOT/_posts/commit"
-# COMMITFIE_PWD = check_output(["echo", ${PWD##*/}])
-# print COMMITFIE_PWD
-print os.path.dirname(os.path.realpath(__file__))
+COMMITFIE_ROOT = HOME+"/.git-commitfie"
+COMMITFIE_IMG_DIR = COMMITFIE_ROOT+"/assets/img/commit"
+COMMITFIE_THUMB_DIR = COMMITFIE_ROOT+"/assets/img/commit/thumbs"
+COMMITFIE_POST_DIR = COMMITFIE_ROOT+"/_posts/commit"
 
 # Set Github and local git variables
 GITHUB_DOMAIN = "https://github.com"
-GIT_LOCAL_USER_NAME = check_output(["git", "config", "user.name"])
-GIT_LOCAL_USER_EMAIL = check_output(["git", "config", "user.email"])
+GIT_LOCAL_USER_NAME = commandCall("git config --global --get user.name")
+GIT_LOCAL_USER_EMAIL = commandCall("git config --global --get user.email")
 
 # Set time of day
-NOW = date.today()
+today = date.today()
+NOW = today.strftime("%Y-%m-%d")
 CURRENT_HOUR = datetime.now().hour
 TIME_OF_DAY = ""
 
@@ -43,19 +58,31 @@ elif CURRENT_HOUR <= 23:
     TIME_OF_DAY = "late evening"
 
 # Get last commit info
-COMMIT_HASH = check_output(["git", "log", "-1", "--pretty=%h"])
-COMMIT_HASH_LONG = check_output(["git", "log", "-1", "--pretty=%H"])
-COMMIT_DATE = check_output(["git", "log", "-1", "--pretty=%ad"])
-COMMIT_MESSAGE = check_output(["git", "log", "-1", "--pretty=%s"])
-COMMIT_BRANCH = check_output(["git", "symbolic-ref", "--short", "HEAD"])
-COMMIT_REPOSITORY_ORIGIN = check_output(["git", "config", "--get", "remote.origin.url"])
-COMMIT_REPOSIROTY_NAME = COMMIT_REPOSITORY_ORIGIN.replace(GITHUB_DOMAIN + '/', '')
+COMMIT_HASH = commandCall("git log -1 --pretty=format:%h")
+COMMIT_HASH_LONG = commandCall("git log -1 --pretty=format:%H")
+COMMIT_DATE = commandCall("git log -1 --pretty=format:%ad")
+COMMIT_DATE_SHORT = commandCall("git log -1 --pretty=format:%ad --date=short")
+COMMIT_MESSAGE = commandCall("git log -1 --pretty=format:%s")
+COMMIT_BRANCH = commandCall("git symbolic-ref --short HEAD")
+COMMIT_REPOSITORY_ORIGIN = commandCall("git config --get remote.origin.url")
+COMMIT_REPOSITORY_NAME = COMMIT_REPOSITORY_ORIGIN.replace(GITHUB_DOMAIN + '/', '')
+print COMMIT_REPOSITORY_NAME
 
 # Set picture and post names
-COMMIT_PICTURE = NOW + "-" + PWD + "-" + COMMIT_HASH
-COMMIT_POST="$COMMIT_PICTURE.markdown"
+COMMIT_PICTURE = COMMIT_DATE_SHORT + "-" + CWD + "-" + COMMIT_HASH + "-" + GIT_LOCAL_USER_NAME
+COMMIT_POST = COMMIT_PICTURE + ".markdown"
 
 # Take the picture and create the thumbnail
+IMAGESNAP_CMD = "imagesnap " + COMMITFIE_IMG_DIR+"/"+COMMIT_PICTURE+".jpg -q -w 1"
+NULL = commandCall(IMAGESNAP_CMD)
+THUMBNAIL_CMD = "convert " + COMMITFIE_IMG_DIR+"/"+COMMIT_PICTURE+".jpg -thumbnail 70x70^ -gravity center -extent 70x70 "+COMMITFIE_THUMB_DIR+"/"+COMMIT_PICTURE+".jpg"
+NULL = commandCall(THUMBNAIL_CMD)
+
+# # Create post
+# if [ -f "$COMMIT_POST_DIR/$COMMIT_POST" ] ; then
+# 	rm $COMMIT_POST_DIR/$COMMIT_POST
+# fi
+# touch $COMMIT_POST_DIR/$COMMIT_POST
 
 # Geotag the commit
 
