da1b58d7c332dc13e547891292319de794ac4eee
ladislas <ladislas@weareleka.com>
2015-01-08 23:02:27 +0100

    sanitize command calls and remove line breaks for git username and emails - start working on geotag - #2 , #8 - fixes #7

--- commitfie.py
+++ commitfie.py
@@ -2,6 +2,7 @@
 
 # Import modules
 import os
+import json
 import subprocess
 from datetime import datetime, date
 
@@ -15,12 +16,10 @@ def commandCall(cmd):
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = process.communicate()
     if (process.returncode == 0):
-        out = out.replace('\n', '').replace('\r', '')
-        print out
+        # print out
         return out
     else:
-        err = err.replace('\n', ' ').replace('\r', '')
-        print err
+        # err = err.replace('\n', ' ').replace('\r', '')
         return "n/a"
 
 # Set Commitfie variables
@@ -31,8 +30,8 @@ COMMITFIE_POST_DIR = COMMITFIE_ROOT+"/_posts/commit"
 
 # Set Github and local git variables
 GITHUB_DOMAIN = "https://github.com"
-GIT_LOCAL_USER_NAME = commandCall("git config --global --get user.name")
-GIT_LOCAL_USER_EMAIL = commandCall("git config --global --get user.email")
+GIT_LOCAL_USER_NAME = commandCall("git config --global --get user.name").replace('\n', '').replace('\r', '')
+GIT_LOCAL_USER_EMAIL = commandCall("git config --global --get user.email").replace('\n', ' ').replace('\r', '')
 
 # Set time of day
 today = date.today()
@@ -66,7 +65,6 @@ COMMIT_MESSAGE = commandCall("git log -1 --pretty=format:%s")
 COMMIT_BRANCH = commandCall("git symbolic-ref --short HEAD")
 COMMIT_REPOSITORY_ORIGIN = commandCall("git config --get remote.origin.url")
 COMMIT_REPOSITORY_NAME = COMMIT_REPOSITORY_ORIGIN.replace(GITHUB_DOMAIN + '/', '')
-print COMMIT_REPOSITORY_NAME
 
 # Set picture and post names
 COMMIT_PICTURE = COMMIT_DATE_SHORT + "-" + CWD + "-" + COMMIT_HASH + "-" + GIT_LOCAL_USER_NAME
@@ -78,12 +76,28 @@ NULL = commandCall(IMAGESNAP_CMD)
 THUMBNAIL_CMD = "convert " + COMMITFIE_IMG_DIR+"/"+COMMIT_PICTURE+".jpg -thumbnail 70x70^ -gravity center -extent 70x70 "+COMMITFIE_THUMB_DIR+"/"+COMMIT_PICTURE+".jpg"
 NULL = commandCall(THUMBNAIL_CMD)
 
+# Geotag the commit
+LAT_LONG = commandCall("whereami")
+GOOGLE_MAPS_CMD = "curl curl http://maps.googleapis.com/maps/api/geocode/json\?latlng\="+LAT_LONG
+GEO_JSON = commandCall(GOOGLE_MAPS_CMD)
+if GEO_JSON != "n/a":
+    results = json.loads(GEO_JSON)['results'][0]['address_components']
+    for address_component in results:
+        if address_component['types'] == ['locality', 'political']:
+            COMMIT_TOWN = address_component['long_name']
+        if address_component['types'] == ['country', 'political']:
+            COMMIT_COUNTRY = address_component['long_name']
+else:
+    COMMIT_TOWN = "@"
+    COMMIT_COUNTRY = "@"
+
+print COMMIT_TOWN
+print COMMIT_COUNTRY
+
 # # Create post
 # if [ -f "$COMMIT_POST_DIR/$COMMIT_POST" ] ; then
 # 	rm $COMMIT_POST_DIR/$COMMIT_POST
 # fi
 # touch $COMMIT_POST_DIR/$COMMIT_POST
-
-# Geotag the commit
 
 # Write the post content
